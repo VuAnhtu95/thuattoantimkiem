c3bde81c37939452e8d556308a81b24ebc5bbb07
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-01-26 11:00:03 +0700

    add

---
+++ src/Stringdodailonnhat.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Stringdodailonnhat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<Character> list = new LinkedList<>();
+        LinkedList<Character> max = new LinkedList<>();
+        System.out.println("Nhập String: ");
+        String string = scanner.nextLine();
+        for (int i = 0; i < string.length(); i++) {
+            if (list.size()>1 && string.charAt(i)<=list.getLast() && list.contains(string.charAt(i))){
+                list.clear();
+            }
+            list.add(string.charAt(i));
+            if (list.size()>max.size()){
+                max.clear();
+                max.addAll(list);
+            }
+        }
+        for (Character i:max) {
+            System.out.println(i);
+        }
+    }
+}
